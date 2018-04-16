ccf17b66088f2aa7197a21fec69e1f95eb4f1ef0
Sophia Swiderski <sophiaswiderski2017@u.northwestern.edu>
2018-04-16 14:11:19 -0500

    updated initialization and rotate function

--- Tile.py
+++ Tile.py
@@ -6,27 +6,19 @@ from tilelist import tilelist
 class Tile:
 	
 	# tile # passed in
-	def __init__(self, tnum):
-
-		#tilenum is the tile # (a way to identify the tile in simple manner)
-		self.tilenum = tnum
-		#contains the actual paths
-		self.paths = tilelist[tnum]
+	def __init__(self, path):
+		#tuple containing the path connections
+		self.path = path
 
 
 	#function to rotate tile (90 degrees clockwise)
 	#assuming tile.paths is a 4-element list of double-element lists 
 
 	def rotate(self):
-
-		#for each double-element list (representing one path in tile)
-		for path in self.paths:
-
-			#for each element of path (representing one end of the path)
-			for end in path:
-
-				#add 2 - representing translating path end 90 degrees clockwise
-				end += 2
-				#if path end is on left side of tile, then restart number
-				if(end >= 8):
-					end -= 8
+		for connection in path:
+			for end in connection
+			#add 2 - representing translating path end 90 degrees clockwise
+			end += 2
+			#if path end is on left side of tile, then restart number
+			if(end >= 8):
+				end -= 8
